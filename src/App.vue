93413e1380eaed11f872b95ef991da44aad22eef
linjunjie <3185058251@qq.com>
2024-02-17 23:39:56 +0800

    #p1-删除无用的部分

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import data from "./data.json"
+import { ref } from "vue"
+import Editor from "./packages/editor.jsx"
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+
+  },
+  setup () {
+    const data = ref(data)
   }
 }
+
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/packages/editor.jsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    // 返回一个渲染函数
+    return () => <div> editor</div>
+  },
+})
