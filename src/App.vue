0d5c8f46a083dbd0b0e17443ddd3f15be85c0149
linjunjie <3185058251@qq.com>
2024-02-18 13:45:22 +0800

    #p1_20240218中午学习部分-基础骨架与样式

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
-<template></template>
+<template>
+  <div class="app">
+    <Editor :data="state"></Editor>
+  </div>
+</template>
 
 <script>
-import data from "./data.json"
-import { ref } from "vue"
-import Editor from "./packages/editor.jsx"
+import data from "./data.json";
+import { ref } from "vue";
+import Editor from "./packages/editor.jsx";
 export default {
   components: {
-
+    Editor,
   },
-  setup () {
-    const data = ref(data)
-  }
-}
-
-
+  setup() {
+    const state = ref(data);
+  },
+};
 </script>
 
 <style lang="scss">
+.app {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  right: 20px;
+  bottom: 20px;
+}
 </style>
--- src/packages/editor.jsx
+++ src/packages/editor.jsx
@@ -1,8 +1,24 @@
 import { defineComponent } from 'vue'
+import "./editor.scss"
 
 export default defineComponent({
+  props: {
+    data:{type:Object}
+  },
   setup() {
     // 返回一个渲染函数
-    return () => <div> editor</div>
+    return () => <div class="editor"> editor
+      <div class="editor-left">左侧物料区</div>
+      <div class="editor-top">菜单栏</div>
+      <div class="editor-right">属性控制栏目</div>
+      <div className="editor-container">
+        {/* 负责产生滚动条 */}
+        <div className="editor-container-canvas">
+          {/* 产生内容区域 */}
+          <div className="editor-container-canvas__content">内容区</div>
+        </div>
+      </div>
+      
+    </div>
   },
 })
